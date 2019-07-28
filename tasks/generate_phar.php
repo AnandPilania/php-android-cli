72ff6681dedff491e387ecdfe0001aa70fd81e01
Anand Pilania <pilaniaanand@gmail.com>
2019-07-28 21:28:45 +0530

    PHAR generating task added

---
+++ tasks/generate_phar.php
@@ -0,0 +1,34 @@
+<?php
+
+define('BASEDIR', dirname(dirname(__FILE__)));
+define('BUILDDIR', BASEDIR . DIRECTORY_SEPARATOR . 'tasks');
+
+$exclude = [
+	'build',
+    '.gitignore',
+    'composer.json',
+    'composer.lock',
+    'composer.phar',
+    'README.md',
+    '.git',
+    '.idea',
+	'tasks'
+];
+
+$filter = function ($file, $key, $iterator) use ($exclude) {
+    if ($iterator->hasChildren() && !in_array($file->getFilename(), $exclude)) {
+        return true;
+    }
+    return $file->isFile() && !in_array($file->getFilename(), $exclude);
+};
+
+$innerIterator = new RecursiveDirectoryIterator(BASEDIR, RecursiveDirectoryIterator::SKIP_DOTS);
+
+$iterator = new RecursiveIteratorIterator(new RecursiveCallbackFilterIterator($innerIterator, $filter));
+
+$phar = new \Phar('phpandroid.phar', 0, 'phpandroid.phar');
+$phar->setSignatureAlgorithm(\Phar::SHA1);
+$phar->startBuffering();
+$phar->buildFromIterator($iterator, BASEDIR);
+$phar->setStub(file_get_contents(BUILDDIR . DIRECTORY_SEPARATOR . 'stub.php'));
+$phar->stopBuffering();
---
+++ tasks/stub.php
@@ -0,0 +1,6 @@
+#!/usr/bin/env php
+<?php
+
+Phar::mapPhar();
+require_once "phar://phpandroid.phar/index.php";
+__HALT_COMPILER();
