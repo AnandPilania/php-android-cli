a396e5e8653c145b02cdc88c2b4cf4605f26a4fc
Banee Ishaque K <Baneeishaque@gmail.com>
2019-08-04 13:15:16 +0530

    Added Gradle Wrapper

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 
 <?php
 
-require_once __DIR__.'/vendor/autoload.php';
-require __DIR__.'/GenerateCommand.php';
+require_once __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/GenerateCommand.php';
 
 $app = new Symfony\Component\Console\Application('Console App', 'v1.0.0');
 $app->add(new GenerateCommand());
