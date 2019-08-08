3e35ba924641e6788e434d361f5b27904d017e28
Anand Pilania <pilaniaanand@gmail.com>
2019-08-08 16:38:41 +0530

    bounded to CLI mode

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
-
 <?php
+
+if (php_sapi_name() !== "cli")
+{
+	echo "PHP Android CLI can only run through CLI.";
+	exit();
+}
 
 require_once __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/GenerateCommand.php';
